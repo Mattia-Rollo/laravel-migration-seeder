3ba77fcacd989f65fe1984cf22f24333ea152ec9
Mattia-Rollo-Portatile <ste.tia@hotmail.it>
2022-12-19 21:03:28 +0100

    add colums in migration file

--- database/migrations/2022_12_19_162607_create_trains_table.php
+++ database/migrations/2022_12_19_162607_create_trains_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda');
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->time('orario_partenza');
+            $table->string('orario_di_arrivo', 50);
+            $table->unsignedTinyInteger('codice_treno', 20);
+            $table->unsignedTinyInteger('numero_carrozze');
+            $table->boolean('in_orario')->nullable()->default('null');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
